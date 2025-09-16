bd8ad6e73e6dbb3c319e33191c4e470f9d37aa1c
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2025-09-16 22:26:54 +0900

    feat: 헤더 목록 일부 추가

--- apps/console-web/src/components/layout/header.tsx
+++ apps/console-web/src/components/layout/header.tsx
@@ -14,11 +14,36 @@ export const Header = () => {
 	const userPicture =
 		'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
-	const storename = '테스트 매장';
+	const currentStore = {
+		name: '테스트 매장',
+		storeId: '1',
+		isOwner: true,
+	};
 
 	const [open, setOpen] = useState(false);
 
-	const storeList = ['테스트 매장', '1번 매장', '2번 매장', '3번 매장'];
+	const storeList = [
+		{
+			name: '테스트 매장',
+			storeId: '1',
+			isOwner: true,
+		},
+		{
+			name: '1번 매장',
+			storeId: '2',
+			isOwner: false,
+		},
+		{
+			name: '2번 매장',
+			storeId: '3',
+			isOwner: false,
+		},
+		{
+			name: '3번 매장',
+			storeId: '4',
+			isOwner: false,
+		},
+	];
 
 	return (
 		<DropdownMenu
@@ -38,44 +63,55 @@ export const Header = () => {
 							<span className="font-bold text-white">{username}</span>
 							<span className="text-neutral-200 text-sm">님!</span>
 						</div>
-						<span className="text-neutral-300 text-xs">{storename}</span>
+						<span className="text-neutral-300 text-xs">
+							{currentStore.name}
+						</span>
 					</div>
 					<ChevronDown className="text-neutral-300 absolute right-4" />
 				</div>
 			</DropdownMenuTrigger>
-			<DropdownMenuContent className="w-60 bg-gradient-to-br from-black/80 to-black/100 text-white backdrop-blur-lg shadow-xl">
-				<DropdownMenuLabel className="text-xs text-neutral-300 flex items-center justify-between">
+			<DropdownMenuContent className="w-60">
+				<DropdownMenuLabel className="flex items-center justify-between">
 					매장 선택
-					<button className="text-xs bg-neutral-100 text-black rounded-full pl-3 pr-2 gap-1 flex items-center">
+					<button className="text-xs hover:bg-neutral-200 bg-neutral-100 text-black rounded-full pl-3 pr-2 gap-1 flex items-center hover:cursor-pointer hover:scale-105 transition-all duration-500">
 						매장 추가
 						<PlusIcon className="w-4" />
 					</button>
 				</DropdownMenuLabel>
-				<DropdownMenuSeparator className="bg-neutral-500" />
+				<DropdownMenuSeparator />
 				{storeList.map((store) => (
 					<DropdownMenuItem
-						key={store}
-						className="transition-all group flex items-center justify-between pl-4"
+						key={store.storeId}
+						className="transition-all flex items-center justify-between pl-4"
 					>
-						{store}
-						{store === storename && (
-							<CheckIcon className="text-white w-4 group-hover:text-black" />
+						<div className="flex">
+							<span>{store.name}</span>
+						</div>
+						{store.storeId === currentStore.storeId && (
+							<CheckIcon className="text-white w-4" />
 						)}
 					</DropdownMenuItem>
 				))}
-				<DropdownMenuLabel className="text-xs text-neutral-300 mt-4">
-					프로필
-				</DropdownMenuLabel>
-				<DropdownMenuSeparator className="bg-neutral-500" />
-				<DropdownMenuItem className="transition-all group pl-4">
+				<DropdownMenuLabel className="mt-4">프로필</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem className="transition-all pl-4">
 					프로필 수정
 				</DropdownMenuItem>
-				<DropdownMenuItem className="transition-all group pl-4">
+				<DropdownMenuItem className="transition-all pl-4">
 					플랜 변경
 				</DropdownMenuItem>
-				<DropdownMenuSeparator className="bg-neutral-500" />
-				<DropdownMenuItem className="flex justify-end text-neutral-400 group">
-					<LogOutIcon className="w-4 text-neutral-300 group-hover:text-black" />
+				<DropdownMenuLabel className="mt-4">고객센터</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem className="transition-all pl-4">
+					이용 가이드
+				</DropdownMenuItem>
+				<DropdownMenuItem className="transition-all pl-4">FAQ</DropdownMenuItem>
+				<DropdownMenuItem className="transition-all pl-4">
+					1:1 문의하기
+				</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem className="flex justify-end text-xs text-neutral-400">
+					<LogOutIcon className="w-4 text-neutral-300" />
 					로그아웃
 				</DropdownMenuItem>
 			</DropdownMenuContent>
